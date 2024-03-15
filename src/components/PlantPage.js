7fd0181f3377c499bd2a1192c1bf47e4226fc3c0
Alvin <alvin.sanchez27@gmail.com>
2024-03-15 00:39:58 -0400

    Completed All Requirements

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
+
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(resp => resp.json())
+    .then(setPlants)
+  }, [])
+
+  const addNewPlant = (newPlant) => {
+    setPlants([...plants], newPlant)
+  }
+
+  const filterPlants = plants.filter((plant) => plant.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant} />
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      <PlantList plants={filterPlants} />
     </main>
   );
 }
